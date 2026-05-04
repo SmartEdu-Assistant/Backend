d9feed8cf064cc63fa18c85abb40497d820315af
Константин Горшков <kgorshkov@kpfu.ru>
2026-05-04 16:53:05 +0300

    fix: config

--- app/core/config.py
+++ app/core/config.py
@@ -1,38 +1,25 @@
 from functools import lru_cache
 
-from pydantic import BaseModel, Field, computed_field
+from pydantic import BaseModel, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from sqlalchemy import URL
 
-BASE_SETTINGS_CONFIG = SettingsConfigDict(
-    env_file='.env',
-    env_file_encoding='utf-8',
-    extra='ignore',
-)
+
+class AppSettings(BaseModel):
+    name: str = 'SmartEdu Assistant API'
+    version: str = '0.1.0'
+    description: str = 'API for the SmartEdu Assistant project'
+    api_v1_prefix: str = '/api/v1'
+    debug: bool = False
 
 
-class AppSettings(BaseSettings):
-    name: str = Field(default='SmartEdu Assistant API', validation_alias='APP_NAME')
-    version: str = Field(default='0.1.0', validation_alias='APP_VERSION')
-    description: str = Field(
-        default='API for the SmartEdu Assistant project',
-        validation_alias='APP_DESCRIPTION',
-    )
-    api_v1_prefix: str = Field(default='/api/v1', validation_alias='API_V1_PREFIX')
-    debug: bool = Field(default=False, validation_alias='DEBUG')
-
-    model_config = BASE_SETTINGS_CONFIG
-
-
-class DatabaseSettings(BaseSettings):
-    echo: bool = Field(default=False, validation_alias='DB_ECHO')
-    host: str = Field(default='127.0.0.1', validation_alias='POSTGRES_HOST')
-    port: int = Field(default=5432, validation_alias='POSTGRES_PORT')
-    name: str = Field(default='smartedu', validation_alias='POSTGRES_DB')
-    user: str = Field(default='postgres', validation_alias='POSTGRES_USER')
-    password: str = Field(default='postgres', validation_alias='POSTGRES_PASSWORD')
-
-    model_config = BASE_SETTINGS_CONFIG
+class DatabaseSettings(BaseModel):
+    echo: bool = False
+    host: str = '127.0.0.1'
+    port: int = 5432
+    name: str = 'smartedu'
+    user: str | None = None
+    password: str | None = None
 
     @computed_field
     @property
@@ -47,14 +34,21 @@ class DatabaseSettings(BaseSettings):
         ).render_as_string(hide_password=False)
 
 
-class Settings(BaseModel):
-    app: AppSettings
-    db: DatabaseSettings
+class Settings(BaseSettings):
+    app: AppSettings = AppSettings()
+    db: DatabaseSettings = DatabaseSettings()
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        extra='ignore',
+        env_nested_delimiter='__',
+    )
 
 
 @lru_cache
 def get_settings() -> Settings:
-    return Settings(app=AppSettings(), db=DatabaseSettings())
+    return Settings()
 
 
 settings = get_settings()
